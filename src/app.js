fa7798aeb333b1561fe707a4d516921f682d7b15
Michał Mazur <michal.mazur@iteo.com>
2020-07-20 20:28:58 +0200

    Create profile functionality

--- src/app.js
+++ src/app.js
@@ -1,29 +1,24 @@
-import './assets/scss/app.scss';
-import $ from 'cash-dom';
-
+import "./assets/scss/app.scss";
+import $ from "cash-dom";
+import axios from "axios";
+import Profile from "./Profile";
 
 export class App {
-  initializeApp() {
-    let self = this;
+  initializeApp = () => {
+    $(".load-username").on("click", () => {
+      const username = $(".username.input").val();
+      if (username) {
+        this.fetchProfileData(username);
+      }
+    });
+  };
 
-    $('.load-username').on('click', function (e) {
-      let userName = $('.username.input').val();
-
-      fetch('https://api.github.com/users/' + userName)
-        .then((response)=> {response.json})
-        .then(function (body) {
-          self.profile = body;
-          self.update_profile();
-        })
-
-    })
-
-  }
-
-  update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
-  }
+  fetchProfileData = (username) => {
+    axios
+      .get(`https://api.github.com/users/${username}`)
+      .then(({ data }) => new Profile(data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 }
